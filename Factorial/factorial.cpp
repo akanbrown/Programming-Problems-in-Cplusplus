bbee84870a8bbd7064afe5ecc8e3bed83a81036e
Akan Brown <akan.brown@stonybrook.edu>
2017-05-31 10:48:57 -0400

    added factorial folder

---
+++ Factorial/factorial.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int factorial(int n){
+    if ((n == 0) || (n == 1)) return 1;
+    else {
+        return (n * factorial(n - 1));
+    }
+
+}
+
+int main()
+{
+	
+	string number, userContinue;
+	int userContinueInt;
+	userContinueInt = 1;
+	
+	while (userContinueInt == 1)
+	{
+		cout << "Enter a number: " << endl;
+		getline(cin, number);
+		cout << "The factorial of " << number << " is " << factorial(stoi(number)) << endl;
+		
+
+		cout << "\nWould you like to compute another factorial? Enter \"yes\" or \"no\"\n" << endl;
+		
+		getline(cin, userContinue);
+
+		if (userContinue == "yes") {
+			userContinueInt = 1;
+		}
+		else if (userContinue == "no"){
+			userContinueInt = 0;
+		}
+		else {
+			cout << "Please enter \"yes\" or \"no\"" << endl;
+		}
+
+  	} 
+		
+	cout << "Thanks for playing." << endl;
+
+	return 0;
+
+}
