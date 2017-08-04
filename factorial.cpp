b086f461545cae2b4d4986b994b44b2faeeb232a
akanbrown <akan.brown@stonybrook.edu>
2017-08-04 07:34:44 -0400

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-int factorial(int n){
-    if ((n == 0) || (n == 1)) return 1;
+int factorial(int num){
+    if ((num == 0) || (num == 1)) return 1;
     else {
-        return (n * factorial(n - 1));
+        return (num * factorial(num - 1));
     }
 
 }
