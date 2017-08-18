7734c65dcaa15af90c6b85daa5c0c5a0a824b2f0
akanbrown <akan.brown@stonybrook.edu>
2017-08-18 10:01:54 -0400

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,50 @@
+/* Implement Bubble Sort in C++ */
+
+#include <iostream>
+using namespace std;
+ 
+// Sort arr[] of size n using Bubble Sort.
+
+void BubbleSort (int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < n-i-1; ++j)
+		{
+			// Comparing consecutive data and switching values if value at j > j+1.
+			if (arr[j] > arr[j+1])
+			{
+				arr[j] = arr[j]+arr[j+1];
+				arr[j+1] = arr[j]-arr[j + 1];
+				arr[j] = arr[j]-arr[j + 1];
+			}
+		}
+		// Value at n-i-1 will be maximum of all the values below this index.
+	}	
+}	
+ 
+int main()
+{
+	int n, i, k;
+	cout << "\nEnter the number of data element to be sorted: ";
+	cin >> n;
+ 
+	int arr[n];
+	
+	for(i = 0; i < n; i++)
+	{
+		cout<<"Enter element "<<i+1<<": ";
+		cin>>arr[i];
+	}
+ 
+	BubbleSort(arr, n);
+	
+    /* Display sorted array: */
+	cout << "\nSorted Array:  ";
+	for (i = 0; i < n; i++)
+        cout<<"  "<<arr[i];
+ 
+    cin >> k;
+	return 0;
+}
