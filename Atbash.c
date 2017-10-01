05b3bc6f4ffb6769d63b154814c600f2fb53d910
akanbrown <akan.brown@stonybrook.edu>
2017-10-01 04:22:24 -0400

    Create Atbash.c

---
+++ Atbash.c
@@ -0,0 +1,19 @@
+// Atbash: A simple substitution cipher.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    FILE *fp;
+    fp = fopen("input.txt", "r");
+    char c;
+    while((c = fgetc(fp)) != EOF) {
+        if((c >= 'a' && c <= 'z') )
+            printf("%c", 'z' - (c - 'a'));
+        else if(c >= 'A' && c <= 'Z')
+            printf("%c", 'Z' - (c - 'A'));
+        else
+            printf("%c", c);
+    }
+    return 0;
+}
